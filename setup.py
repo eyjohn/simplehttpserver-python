5c2e292895a8f198e293afb6b40190eaa1d3abba
Evgeny Yakimov <john.yakimov@gmail.com>
2019-12-30 16:08:31 +0000

    Improved setup.py and Makefile

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
 
-sourcefiles = ['simplehttp/ext.pyx']
-
-extensions = [Extension("simplehttp.ext", sourcefiles, language='c++')]
+extensions = [Extension("simplehttp.ext", ['simplehttp/ext.pyx'], language='c++')]
 
 setup(
     name="simplehttp",
+    packages=["simplehttp"],
+    version='1.0',
+    description='A native C++ HTTP Client and Server with inter-platform OpenTracing support.',
+    author='Evgeny Yakimov',
+    author_email='evgeny@evdev.me',
+    url='https://github.com/eyjohn/otinterop',
     ext_modules=cythonize(extensions, language_level=3)
 )
