c34e9af1c4de1b31d78c452f7704cde05c6dfc49
Evgeny Yakimov <john.yakimov@gmail.com>
2020-01-07 22:42:36 +0000

    Switch to W3C c++ tracer

--- src/otinterop_tracer.h
+++ src/otinterop_tracer.h
@@ -1,20 +1,15 @@
 #include "otinterop_span.h"
 
-#include <opentracing/tracer.h>
+#include <w3copentracing/tracer.h>
 #include <opentracing/span.h>
-#include <opentracing/propagation.h>
 #include <opentracing/string_view.h>
 
 #include <memory>
-#include <string>
-#include <map>
-#include <optional>
 #include <iostream>
-#include <functional>
 
 namespace otinterop {
 
-class Tracer: public opentracing::Tracer {
+class Tracer: public w3copentracing::Tracer {
  public:
 
   Tracer();
@@ -23,26 +18,14 @@ class Tracer: public opentracing::Tracer {
       opentracing::string_view operation_name, const opentracing::StartSpanOptions& options) const
       noexcept override;
 
-  using opentracing::Tracer::Extract;
-  using opentracing::Tracer::Inject;
+  using w3copentracing::Tracer::Extract;
+  using w3copentracing::Tracer::Inject;
 
   opentracing::expected<void> Inject(const opentracing::SpanContext& sc,
                                      std::ostream& writer) const override;
 
-  opentracing::expected<void> Inject(const opentracing::SpanContext& sc,
-                                     const opentracing::TextMapWriter& writer) const override;
-
-  opentracing::expected<void> Inject(const opentracing::SpanContext& sc,
-                                     const opentracing::HTTPHeadersWriter& writer) const override;
-
   opentracing::expected<std::unique_ptr<opentracing::SpanContext>> Extract(
       std::istream& reader) const override;
-
-  opentracing::expected<std::unique_ptr<opentracing::SpanContext>> Extract(
-      const opentracing::TextMapReader& reader) const override;
-
-  opentracing::expected<std::unique_ptr<opentracing::SpanContext>> Extract(
-      const opentracing::HTTPHeadersReader& reader) const override;
 
   void Close() noexcept override;
 
