5545f77a3a9123b6fd6729019eb83aab50b259f2
Evgeny Yakimov <john.yakimov@gmail.com>
2020-01-09 23:44:11 +0000

    Improve testtracer and test case
    The parent context, which can come from scope, is now
    added to the references

--- testtracer/tracer.py
+++ testtracer/tracer.py
@@ -1,17 +1,17 @@
-import w3copentracing
+from w3copentracing import generate_span_id, generate_trace_id, SpanContext, Tracer as W3CTracer
 import opentracing
 import time
 from opentracing.scope_managers import ThreadLocalScopeManager
 from .span import Span
 
 
-class Tracer(w3copentracing.Tracer):
+class Tracer(W3CTracer):
     def __init__(self, scope_manager=None):
         """Initialize a MockTracer instance."""
         scope_manager = ThreadLocalScopeManager() \
             if scope_manager is None else scope_manager
 
-        w3copentracing.Tracer.__init__(self, scope_manager)
+        W3CTracer.__init__(self, scope_manager)
 
     def start_active_span(self,
                           operation_name,
@@ -41,6 +41,7 @@ class Tracer(w3copentracing.Tracer):
 
         start_time = time.time() if start_time is None else start_time
 
+        # Work out parent context from parameters (reference/child_of)
         parent_ctx = None
         if child_of is not None:
             parent_ctx = (
@@ -49,20 +50,27 @@ class Tracer(w3copentracing.Tracer):
         elif references is not None and len(references) > 0:
             parent_ctx = references[0].referenced_context
 
+        # Can we deduce parent_ctx from scope?
         if not ignore_active_span and parent_ctx is None:
             scope = self.scope_manager.active
             if scope is not None:
                 parent_ctx = scope.span.context
 
-        ctx = w3copentracing.SpanContext(
-            span_id=w3copentracing.generate_span_id())
-
+        ctx = SpanContext(span_id=generate_span_id())
         if parent_ctx is not None:
+            # Deduce trace_id from parent
             if parent_ctx.baggage is not None:
                 ctx.baggage = parent_ctx.baggage.copy()
             ctx.trace_id = parent_ctx.trace_id
+
+            # Make sure parent ctx is included in references, or add it
+            if (references is None or
+                    parent_ctx not in [r.referenced_context for r in references]):
+                if references is None:
+                    references = []
+                references = [opentracing.child_of(parent_ctx)]
         else:
-            ctx.trace_id = w3copentracing.generate_trace_id()
+            ctx.trace_id = generate_trace_id()
 
         return Span(collector=print,
                     tracer=self,
